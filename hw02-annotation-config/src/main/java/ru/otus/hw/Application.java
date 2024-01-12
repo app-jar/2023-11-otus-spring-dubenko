ea290ff4bd54cd771d05fe2daf8f953933462ac0
app-jar <cofee.jar@yandex.ru>
2024-01-12 17:04:54 +0300

    recommendations

--- hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/Application.java
@@ -2,8 +2,11 @@ package ru.otus.hw;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.test.TestRunnerService;
 
+
+@PropertySource("classpath:application.properties")
 @ComponentScan
 public class Application {
     public static void main(String[] args) {
