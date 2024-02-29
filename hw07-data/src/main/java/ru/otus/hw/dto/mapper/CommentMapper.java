87a05e126d2c34d4a52c2638a060eb65037d3f7b
app-jar <cofee.jar@yandex.ru>
2024-03-01 01:33:25 +0300

    resolve conversations

--- hw07-data/src/main/java/ru/otus/hw/dto/mapper/CommentMapper.java
+++ hw07-data/src/main/java/ru/otus/hw/dto/mapper/CommentMapper.java
@@ -2,15 +2,10 @@ package ru.otus.hw.dto.mapper;
 
 import ru.otus.hw.dto.CommentDto;
 import ru.otus.hw.models.Comment;
-import ru.otus.hw.models.view.CommentView;
 
 public class CommentMapper {
 
     public static CommentDto toDto(Comment comment) {
-        return new CommentDto(comment.getId(), comment.getBook().getTitle(), comment.getText());
-    }
-
-    public static CommentDto toDto(CommentView comment) {
         return new CommentDto(comment.getId(), comment.getBook().getTitle(), comment.getText());
     }
 
